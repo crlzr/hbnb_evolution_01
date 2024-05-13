f49c28c3bddff2d7c7d298df8498927665e57341
Adrian <dreamweaver3d@gmail.com>
2024-05-14 07:53:51 +0800

    added a few tests

--- part01/tests/test_city.py
+++ part01/tests/test_city.py
@@ -13,17 +13,42 @@ class TestCity(unittest.TestCase):
     """
 
     def test_create_city(self):
-        """Tests creation of City instances
-        """
+        """Tests creation of City instances """
+
         # Note that this test only works if the test country data is loaded
         # don't forget to include the TESTING = 1 flag at the command line
-        # TESTING=1 python3 -m unittest discover
+        # type in the terminal: TESTING=1 python3 -m unittest discover
         c = City(name="Vancouver", country_id="d291a77f-fa95-4385-b70e-2691df246475")
 
         self.assertIsNotNone(c)
 
-    # TODO: add more tests
+    def test_create_city_invalid_name(self):
+        """ Tests error handling during creation of City instances """
 
+        error = 0
+        try:
+            City(name="@#$%^&", country_id="d291a77f-fa95-4385-b70e-2691df246475")
+        except ValueError:
+            error = 1
+
+        # We're not able to use self.assertRaises here since it can't take kwargs
+        self.assertEqual(error, 1)
+
+
+    def test_create_city_invalid_country_id(self):
+        """ Tests error handling during creation of City instances """
+
+        error = 0
+        try:
+            City(name="Wakanda", country_id="616")
+        except ValueError:
+            error = 1
+
+        # We're not able to use self.assertRaises here since it can't take kwargs
+        self.assertEqual(error, 1)
+
+
+    # TODO: add more tests
 
 if __name__ == '__main__':
     unittest.main()
