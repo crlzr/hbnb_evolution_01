7984f52c9861017b0bc86a91f1e9056691e22ce7
Carole <c.zenruffinen@hotmail.com>
2024-06-13 15:17:18 +1000

    added tests

--- tests/test_review.py
+++ tests/test_review.py
@@ -20,9 +20,9 @@ class TestReview(unittest.TestCase):
 
         self.assertIsNotNone(r)
 
-    def feedback_is_string(self):
+    def test_feedback_not_string(self):
         """
-        Tests if feedback is wrong type
+        Tests if feedback is not string
         """
         test_error = ""
         try:
@@ -30,10 +30,40 @@ class TestReview(unittest.TestCase):
                     place_id="cee845de-c341-4f5a-a0c5-2ca1f4c327b2",
                     feedback=999999999,
                     rating=4)
-        except TypeError as e:
+        except Exception as e:
             test_error = e
 
         self.assertIsInstance(test_error, TypeError)
+
+    def test_rating_not_int(self):
+        """
+        Tests if rating is not int
+        """
+        test_error = ""
+        try:
+            Reviews(commentor_user_id="0215a722-a3fc-4f08-9120-f8621147f2be",
+                    place_id="cee845de-c341-4f5a-a0c5-2ca1f4c327b2",
+                    feedback="If I could give 0 stars, I would",
+                    rating="If I could give 0 stars, I would")
+        except Exception as e:
+            test_error = e
+
+        self.assertIsInstance(test_error, TypeError)
+
+    def test_rating_wrong_value(self):
+        """
+        Tests if rating is wrong value (< 1 or > 5)
+        """
+        test_error = ""
+        try:
+            Reviews(commentor_user_id="0215a722-a3fc-4f08-9120-f8621147f2be",
+                    place_id="cee845de-c341-4f5a-a0c5-2ca1f4c327b2",
+                    feedback="If I could give 0 stars, I would",
+                    rating=0)
+        except Exception as e:
+            test_error = e
+
+        self.assertIsInstance(test_error, ValueError)
 
 
 
